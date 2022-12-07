7972469e32569cde62da4083650e8c2e16b412e9
Liudmyla Dubravska <liudmyla.sikaliuk@gmail.com>
2022-12-07 14:00:00 +0200

    updated Main

--- HomeWorkOOP/src/Main.java
+++ HomeWorkOOP/src/Main.java
@@ -2,11 +2,7 @@ import Employee.Employee;
 
 public class Main {
     public static void main(String[] args) {
-        String name;
-        String position;
-        String email;
-        String phoneNumber;
-        int age;
+
         Employee empl1 = new Employee("Jenna Ortega", "Actress", "jenna.ortega@gmail.com", "+1424-3222-9331", 20);
 
         System.out.println("Employee name: " + empl1.getName());
