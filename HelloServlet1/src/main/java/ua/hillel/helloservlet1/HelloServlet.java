456cda76c365bb7d670d249ac6e40c2f17f71762
Liudmyla Dubravska <liudmyla.sikaliuk@gmail.com>
2023-05-24 10:06:54 +0300

    update Hello Servlet

--- HelloServlet1/src/main/java/ua/hillel/helloservlet1/HelloServlet.java
+++ HelloServlet1/src/main/java/ua/hillel/helloservlet1/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Не беси меня сервлет!";
+        message = "Hello Servlet!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
